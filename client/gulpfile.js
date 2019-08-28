4afd2c241883dafcd52e26156f9ec6801bdbfe1c
oToToT <ty1208chiang@gmail.com>
2019-08-28 17:34:13 +0800

    move babel into webpack

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -3,7 +3,6 @@ const pug = require('gulp-pug');
 const less = require('gulp-less');
 const webpack = require('webpack-stream');
 const named = require('vinyl-named');
-const babel = require('gulp-babel');
 const minifyCSS = require('gulp-csso');
 const concat = require('gulp-concat');
 const uglifyJS = require('gulp-uglify');
@@ -26,14 +25,28 @@ function css() {
 
 function js() {
     return src(['javascript/*.js', '!javascript/config.js'])
-        .pipe(babel({
-            presets: ['@babel/env'],
-            plugins: ['@babel/transform-runtime']
-        }))
         .pipe(named())
-        .pipe(webpack({mode: 'production'}))
-        .pipe(babel({
-            presets: ['@babel/env']
+        .pipe(webpack({
+            mode: 'production',
+            module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        loader: 'babel-loader',
+                        query: {
+                            presets: [
+                                [
+                                    '@babel/env',
+                                    {
+                                        "targets": "> 0.2%, not dead"
+                                    }
+                                ]
+                            ],
+                            plugins: ['@babel/transform-runtime']
+                        }
+                    }
+                ]
+            }
         }))
         .pipe(uglifyJS())
         .pipe(rename(function(path){
